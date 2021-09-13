9d5479015d7e4519e39550f6006b526438621713
Nick Yozov <n_yozov@hotmail.com>
2021-09-13 19:06:31 +0000

    Added assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual(1, 2);
