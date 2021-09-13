1cb69586788ce51e48fcac38b80b97310bd42336
Nick Yozov <n_yozov@hotmail.com>
2021-09-13 19:32:37 +0000

    Added head.js

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = (arr) => {
+  return arr[0]
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
