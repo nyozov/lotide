9f450346242e2f647354d470aabee728f7bf6676
Nick Yozov <n_yozov@hotmail.com>
2021-09-15 18:41:03 +0000

    added letterPositions

---
+++ letterPositions.js
@@ -0,0 +1,43 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (!results[sentence[i]]) {
+        results[sentence[i]] = [i];
+
+      } else {
+        results[sentence[i]].push(i);
+      }
+    }
+
+  }
+  return results;
+};
+
+console.log(letterPositions("hello"));
+
+
+
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`🟢 Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+assertArraysEqual(letterPositions("hello").e, [1]);
