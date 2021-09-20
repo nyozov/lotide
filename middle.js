034497c6701df1f4b64ef60f64a2c736186355f3
Nick Yozov <n_yozov@hotmail.com>
2021-09-20 17:24:49 +0000

    updated middle.js

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`🟢 Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🔴 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 const middle = (arr) => {
   let newArr = [];
   if (arr.length > 2) {
@@ -30,11 +10,6 @@ const middle = (arr) => {
   }
   return newArr;
 };
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
 
-assertArraysEqual([2, 3], middle([1, 2, 3, 4]));
+
+module.exports = middle;
