36bee72ecf7e871c4ad8fd02cef2cdfa535d74ff
Nick Yozov <n_yozov@hotmail.com>
2021-09-20 19:07:48 +0000

    fixed bug

--- middle.js
+++ middle.js
@@ -2,10 +2,10 @@ const middle = (arr) => {
   let newArr = [];
   if (arr.length > 2) {
     if (arr.length % 2 !== 0) {
-      newArr.push(arr[Math.round(arr.length / 2)]);
+      newArr.push(arr[Math.floor(arr.length / 2)]);
     } else {
-      newArr.push(arr[Math.round(arr.length / 2)] - 1);
-      newArr.push(arr[Math.round(arr.length / 2)]);
+      newArr.push(arr[Math.floor(arr.length / 2)] - 1);
+      newArr.push(arr[Math.floor(arr.length / 2)]);
     }
   }
   return newArr;
