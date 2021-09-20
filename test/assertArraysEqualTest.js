7b7767b098d57223439ff1cf56870360742c98f7
Nick Yozov <n_yozov@hotmail.com>
2021-09-20 17:14:09 +0000

    added assertArraysEqualTest.js

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+const assertArraysEqual = require("../assertArraysEqual");
+
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 2]);
+assertArraysEqual([1, 2, 3], [1, 2, '3']);
